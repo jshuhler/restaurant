64530273e68ca136b5b38e408ec7026b1ae44f74
jshuhler <15068877+jshuhler@users.noreply.github.com>
2026-04-16 18:23:07 -0400

    menu html written, working on js

---
+++ src/menu.js
@@ -0,0 +1,9 @@
+
+
+export const menuPage = () => {
+    // menu title
+    let menuTitle = document.createElement("p");
+    menuTitle.classList.add("menu-title");
+    menuTitle.textContent = "Our Current Weiner Offerings";
+}
+
